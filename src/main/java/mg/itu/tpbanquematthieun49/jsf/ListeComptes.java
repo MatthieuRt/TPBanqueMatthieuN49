b32a4046dca40c8e48f5c09a52b9f2212b93fecf
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-04-11 23:39:33 +0300

    Page qui liste les comptes et menu

---
+++ src/main/java/mg/itu/tpbanquematthieun49/jsf/ListeComptes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquematthieun49.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquematthieun49.entity.CompteBancaire;
+import mg.itu.tpbanquematthieun49.service.GestionnaireCompte;
+
+/**
+ *
+ * @author Matthieu R
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable{
+
+    @Inject
+    GestionnaireCompte gestionnaireCompte;
+    
+    private List<CompteBancaire> compteBancaires;
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+    
+    public List<CompteBancaire> getAllComptes(){
+        if(compteBancaires==null){
+            compteBancaires = gestionnaireCompte.getAllComptes();
+        }
+        return compteBancaires;
+    }
+    
+}
