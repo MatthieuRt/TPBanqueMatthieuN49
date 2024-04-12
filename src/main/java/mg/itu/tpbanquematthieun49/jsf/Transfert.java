a8d865a2bf185c89194111afc8232c58973fd371
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-04-12 21:50:00 +0300

    Traiter les erreurs et messages de succès

--- src/main/java/mg/itu/tpbanquematthieun49/jsf/Transfert.java
+++ src/main/java/mg/itu/tpbanquematthieun49/jsf/Transfert.java
@@ -7,6 +7,8 @@ package mg.itu.tpbanquematthieun49.jsf;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import mg.itu.tpbanquematthieun49.entity.CompteBancaire;
+import mg.itu.tpbanquematthieun49.jsf.util.Util;
 import mg.itu.tpbanquematthieun49.service.GestionnaireCompte;
 
 /**
@@ -19,11 +21,11 @@ public class Transfert {
 
     @Inject
     GestionnaireCompte gestionnaireCompte;
-    
+
     private Long idSource;
     private Long idDestination;
     private int montant;
-    
+
     /**
      * Creates a new instance of Transfert
      */
@@ -62,8 +64,33 @@ public class Transfert {
         this.montant = montant;
     }
 
-    public String transferer(){
-        gestionnaireCompte.transfererArgent(idSource, idDestination, montant);
+    public String transferer() {
+        boolean erreur = false;
+        CompteBancaire source = gestionnaireCompte.findById(idSource);
+        CompteBancaire destination = gestionnaireCompte.findById(idDestination);
+        if (source == null || destination == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            if(source==null){
+                Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            }
+            if(destination==null){
+                Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+            }
+             erreur = true;
+        } else {
+            if (source.getSolde() < montant) { 
+                erreur = true;
+                Util.messageErreur("Solde insuffisant pour le transfert !");
+            }
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+        gestionnaireCompte.transfererArgent(idSource, idDestination, montant);        // Message de succès ; addFlash à cause de la redirection.
+        // ...Complétez pour faire apparaitre le montant et les noms des 2 propriétaires des comptes.
+        Util.addFlashInfoMessage("Transfert correctement effectué, source : "+idSource+",destination : "+idDestination+",montant : "+montant);
         return "listeComptes?faces-redirect=true";
     }
 }
